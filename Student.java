2e483072252c0e55eadaeb77b357e6d617a65ba2
gandalf3 <gandalf3@blendermonkey.com>
2017-03-21 13:19:11 -0700

    initial commit

---
+++ Student.java
@@ -0,0 +1,20 @@
+import java.util.Date;
+import java.text.DateFormat;
+import java.util.ArrayList;
+
+public class Student {
+	String name;
+	double age;
+	int grade;
+	double gpa;
+
+	class Class {
+		String title;
+	}
+	/*
+	public void addClass();
+	public void dropClass();
+
+	public Student() {}
+	*/
+}
---
+++ StudentManager.java
@@ -0,0 +1,10 @@
+public class StudentManager {
+
+	public static void main(String[] args) {
+		Student s = new Student();
+
+		System.out.println(s.name);
+
+	}
+
+}
